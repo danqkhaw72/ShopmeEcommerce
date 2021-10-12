f138c9658369650f66711df8dce523c039832273
Dang Kham <danqkhaw72@gmail.com>
2021-10-12 15:07:15 +0700

    Code user entity, role repository and unit test

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateNewUserWithOneRoles() {
+		Role roleAdmin = entityManager.find(Role.class, 1);
+		User userKamu = new User("kamu72@gmail.com", "kamu123", "Kham", "Nguyen Dang");
+		userKamu.addRole(roleAdmin);
+		
+		User savedUser =  repo.save(userKamu);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateNewUserWithTwoRoles() {
+		User userRavi = new User("ravi@gmail.com", "ravi123", "Ravi", "Mia");
+		Role roleEditor = new Role(3);
+		Role roleAssistant = new Role(5);
+		
+		userRavi.addRole(roleEditor);
+		userRavi.addRole(roleAssistant);
+		
+		User savedUser =  repo.save(userRavi);
+		
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testListAllUsers() {
+		Iterable<User> listUsers = repo.findAll();
+		listUsers.forEach(user -> System.out.println(user));
+	}
+}
